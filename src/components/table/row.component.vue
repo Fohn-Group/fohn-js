e6ebd2595c721beda920376b887b1d5153744f52
Alain Belair <belair.alain@gmail.com>
2023-08-25 08:47:15 -0400

    add selection to row component

--- src/components/table/row.component.vue
+++ src/components/table/row.component.vue
@@ -1,26 +1,44 @@
-<template>
-    <slot :row="row" v-bind="$attrs">row</slot>
-</template>
-
 <script>
+import {computed, inject, toRefs} from "vue";
+import {useTableStoreFactory} from "./table.store";
+
 export default {
   name: 'fohn-table-row',
   props: {
+    // storeId: String,
+    hasSelection: {
+      type: Boolean,
+      default: false,
+    },
     row: Object,
   },
   emits: {},
   setup: function (props, { attrs, slots, emit }) {
-    const { row } = props;
-    const method = {};
+    const { hasSelection: isActionable } = props;
+    const { row } = toRefs(props);
+    const tableStore = useTableStoreFactory(inject('tableStoreId'))();
 
-    method.getRowClass = (idx) => ({
-      'bg-gray-200': idx % 2,
-    });
 
-    return { row };
+    const isEvenRow = (idx) => (idx % 2) === 0;
+    const isSelected = computed(() => tableStore.isRowSelected(row.value.id));
+
+    const toggleRow = (id) => {
+      tableStore.toggleRow(id);
+    }
+    return { row, isEvenRow, isActionable, isSelected, toggleRow };
   },
 };
 </script>
+
+<template>
+  <slot
+      :row="row"
+      :isEvenRow="isEvenRow"
+      :isActionable="isActionable"
+      :isSelected="isSelected"
+      :toggleRow="toggleRow"
+      v-bind="$attrs">row</slot>
+</template>
 
 <style scoped>
 
