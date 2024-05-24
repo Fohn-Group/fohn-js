c35426df944a7a921e4e9aa09e66ecc61c0fa845
Alain Belair <belair.alain@gmail.com>
2024-05-24 14:50:24 -0400

    init filter table component

---
+++ src/components/table/table.filter.component.vue
@@ -0,0 +1,24 @@
+<script>
+import {computed, inject, toRefs} from "vue";
+import {toReactive} from "@vueuse/core";
+import {useTableStoreFactory} from "./table.store";
+
+export default {
+  name: 'fohn-table-filter',
+  props: {
+  },
+
+  setup(props, { attrs, slots, emit }) {
+
+  }
+}
+
+</script>
+
+<template>
+  <slot v-bind="$attrs">table action</slot>
+</template>
+
+<style scoped>
+
+</style>
