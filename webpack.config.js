30dfe73d5e4f54b2e401fb4fe11f0ffbbe47d16a
Alain Belair <belair.alain@gmail.com>
2024-05-24 14:50:12 -0400

    fix Vue warning

--- webpack.config.js
+++ webpack.config.js
@@ -116,6 +116,7 @@ module.exports = (env) => {
         __VERSION__: JSON.stringify(packageVersion),
         __VUE_OPTIONS_API__: true,
         __VUE_PROD_DEVTOOLS__: false,
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
       }),
       new VueLoaderPlugin(),
     ],
