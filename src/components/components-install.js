d5f99b0dfe9a91138eae7de514376d140baabdc4
Alain Belair <belair.alain@gmail.com>
2024-05-24 14:50:30 -0400

    Update components-install.js

--- src/components/components-install.js
+++ src/components/components-install.js
@@ -14,6 +14,7 @@ import TableCell from './table/cell.component.vue';
 import ExceptionModal from './modal/exception-modal.component.vue';
 import TablePaginator from './table/paginator.component.vue';
 import TableAction from "./table/table.action.component.vue";
+import TableFilter from "./table/table.filter.component.vue";
 import Dummy from './dummy.component.vue';
 import Modal from './modal/modal.component.vue';
 import Tabs from './tabs/tabs.component.vue';
@@ -35,6 +36,7 @@ const fohnComponents = [
   {name: 'fohn-table-cell', def:  TableCell},
   {name: 'fohn-table-paginator', def:  TablePaginator},
   {name: 'fohn-table-action', def:  TableAction},
+  {name: 'fohn-table-filter', def:  TableFilter},
   {name: 'fohn-modal', def:  Modal},
   {name: 'fohn-ui-exception', def:  ExceptionModal},
   {name: 'fohn-tab', def:  Tab},
