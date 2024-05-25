d52086e599d660743a1f333bbe3f8661a0f0d411
Alain Belair <belair.alain@gmail.com>
2024-05-25 11:35:42 -0400

    added vue prop and event

--- src/components/table/table.filter.component.vue
+++ src/components/table/table.filter.component.vue
@@ -1,22 +1,51 @@
 <script>
-import {computed, inject, toRefs} from "vue";
-import {toReactive} from "@vueuse/core";
+import {computed, inject, ref} from "vue";
 import {useTableStoreFactory} from "./table.store";
 
 export default {
   name: 'fohn-table-filter',
   props: {
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+    iconName: String,
+    altIconName: String,
   },
 
   setup(props, { attrs, slots, emit }) {
+    const {iconName, altIconName} = props;
+    const isActive = ref(props.isActive);
 
+    const tableStore = useTableStoreFactory(inject('tableStoreId'))();
+    const tableIsFetching = ref(false);
+
+    tableStore.$subscribe((mutation, state) => {
+      tableIsFetching.value = state.isFetching;
+    });
+
+    const iconCss = computed(() => ({
+      [iconName]: !isActive.value,
+      [altIconName]: isActive.value,
+      'text-gray-200': tableIsFetching.value,
+    }));
+
+    const toggleFilterIcon = () => isActive.value = !isActive.value;
+
+    return {
+      iconCss,
+      toggleFilterIcon,
+    }
   }
 }
 
 </script>
 
 <template>
-  <slot v-bind="$attrs">table action</slot>
+  <slot
+      :iconCss=iconCss
+      :toggleFilterIcon=toggleFilterIcon
+      v-bind="$attrs">table filter</slot>
 </template>
 
 <style scoped>
