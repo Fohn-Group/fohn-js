55b1194cddfb148d8cabc38f9a99acc210a217a2
Alain Belair <belair.alain@gmail.com>
2024-06-05 09:42:10 -0400

    add utility fn

---
+++ src/components/utils/composable/utils.js
@@ -0,0 +1,17 @@
+
+
+/**
+ * Return an array index matching the testing function (fn)
+ * If no index is found, then return a default one.
+ *
+ */
+const useFindIndexDefault = (ar, fn, defaultIdx = 0) => {
+  let idx = ar.findIndex(fn);
+  if (idx === -1) {
+    idx = defaultIdx;
+  }
+
+  return idx;
+}
+
+export { useFindIndexDefault }
