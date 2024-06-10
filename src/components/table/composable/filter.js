6339927bb58a7e03fa9f1beb20888e8ed8f8230c
Alain Belair <belair.alain@gmail.com>
2024-06-10 15:21:01 -0400

    Update filter.js

--- src/components/table/composable/filter.js
+++ src/components/table/composable/filter.js
@@ -3,6 +3,9 @@
  * Return filter default value using the first column and the first available operator
  * for this first column.
  */
+
+import isEqual from "lodash.isequal";
+
 function useDefaultFilterValue(columns, operators) {
   const columnDef = columns[0];
 
@@ -13,8 +16,31 @@ function useDefaultFilterValue(columns, operators) {
   return {
     column: columnDef.id,
     operator: columnOperators[0].id,
-    value: '',
+    requiredValue: columnOperators[0].requiredValue,
+    value: null,
   }
 }
 
-export {useDefaultFilterValue}
+function useIsFiltersEqual(filter1, filter2) {
+  // check for length first.
+  if (filter1.length !== filter2.length) {
+    return false;
+  }
+
+  const copyF1 = [...filter1];
+  const copyF2 = [...filter2];
+  let isEqualValue = true;
+  copyF1.sort( (a,b) => a.filterId - b.filterId);
+  copyF2.sort( (a,b) => a.filterId - b.filterId);
+
+  for (let i = 0; i < copyF1.length; i++) {
+    if (!isEqual(copyF1[i],copyF2[i])) {
+      isEqualValue = false;
+      break;
+    }
+  }
+
+  return isEqualValue;
+}
+
+export {useDefaultFilterValue, useIsFiltersEqual}
